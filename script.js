1677226b87049d6e0c8da67b947ebdaf9e1eeb5c
Sviatlana Askirka <svet.oskirko@gmail.com>
2022-09-02 18:25:17 +0300

    Домашнее задание 5
    При клике на кнопку "Предсказать судьбу на сегодня" (я добавила "на сегодня") генерируется предсказание;
    Предсказание выводится с вероятностью в процентах;
    При генерации нового предсказания, старое добавляется в начало списка "Мои предсказания".

--- script.js
+++ script.js
@@ -13,3 +13,54 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+
+const button = document.querySelector('.forecast-btn');
+const forecastTitle = document.querySelector('.current-forecast h1');
+const forecastContainer = document.querySelector('.forecasts');
+const forecastTemplate = document.querySelector('#forecast-item');
+const forecastChance = document.querySelector('.current-forecast p');
+
+
+const firstForecast = 'Хорошие новости';
+const secondForecast = 'Финансовый успех';
+const thirdForecast = 'Встреча с важным человеком';
+const fourthForecast = 'Неудача';
+
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function addForecastByTemplate() {
+    const myForecast = forecastTemplate.content.cloneNode(true);
+    myForecast.querySelector('h3').textContent = forecastTitle.textContent;
+    myForecast.querySelector('p').textContent = forecastChance.textContent;
+    return myForecast;
+}
+
+button.addEventListener('click', function() {
+    if (forecastTitle.textContent && forecastChance.textContent) {
+        const myForecasts = addForecastByTemplate();
+        forecastContainer.prepend(myForecasts);
+    }
+
+    let random = getRandomValue(0, 5);
+
+    switch (random) {
+        case 0:
+            forecastTitle.textContent = firstForecast
+            break;
+        case 1:
+            forecastTitle.textContent = secondForecast
+            break;
+        case 2:
+            forecastTitle.textContent = thirdForecast
+            break;
+        case 3:
+            forecastTitle.textContent = fourthForecast
+            break;
+    }
+
+    let amount = getRandomValue(0, 100);
+    forecastChance.textContent = 'Вероятность: ' + amount + '%';
+})
